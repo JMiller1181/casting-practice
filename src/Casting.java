4c200af0d2bccd152e7eeacfd594677c17eb2486
Jacob Miller <millerjr1181@gmail.com>
2023-05-08 16:35:29 -0400

    Created variables and casted them

---
+++ src/Casting.java
@@ -0,0 +1,13 @@
+public class Casting {
+    public static void main (String[] args) {
+        double number = 112.35;
+        int num = (int)number;
+        System.out.println("Double: " + number);
+        System.out.println("Int: " + num);
+        String sevenSeven = "49";
+        int sevenSquare = Integer.parseInt(sevenSeven);
+        System.out.println("The string value is: " + sevenSeven);
+        System.out.println("The integer value is: " + sevenSquare);
+
+    }
+}
